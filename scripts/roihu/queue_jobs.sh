90d48269481678806de4c272df7d261f62e0d95b
dancaseb <danielisreeves@gmail.com>
2026-08-27 17:34:45 +0300

    add better job scheduling

---
+++ scripts/roihu/queue_jobs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+ML=4
+gpus=4
+
+for N in 4 8 16 32 64 288; do
+    cpus=$((gpus * 72 / N))
+    # cpus=1
+    sbatch --job-name="run_Elmer_roihu_N1_n${N}_c${cpus}_ML${ML}" \
+           --nodes=1 \
+           --exclusive \
+           --time="00:45:00" \
+           --output="logs/%x_%j.out" \
+           --error="logs/%x_%j.err" \
+           --mem=0 \
+           --gres=gpu:gh200:${gpus} \
+           --cpus-per-task=${cpus} \
+           --ntasks-per-node=${N} \
+           --partition=gpumedium \
+           --account=project_2001659 \
+           run.sh ${ML}
+done
--- scripts/roihu/run_mps.sh
+++ scripts/roihu/run_mps.sh
@@ -1,19 +1,6 @@
 #!/bin/bash -l
-#SBATCH --job-name=run_Elmer_roihu_N1_n72_c1_ML1_MPS
-#SBATCH --account=project_2001659
-#SBATCH --partition=gputest
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=72
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:gh200:4
-#SBATCH --time=00:15:00
-#SBATCH --mem=0
-#SBATCH --exclusive
-#SBATCH --output=%x_%j.out
-#SBATCH --error=%x_%j.err
-
 # MESH LEVEL
-export MESH_LEVEL="1"
+export MESH_LEVEL=$1
 
 # DIR PATHS
 export BASEDIR="/scratch/project_2001659/danieree/rsync/my_ElmerIceEnergy"
